168647359e7b20d284ff3aef02129f65d87eaa7b
Josh Lindsey <joshua.s.lindsey@gmail.com>
2015-04-06 14:21:21 -0400

    Install paper trail

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 # Main user class
 class User < ActiveRecord::Base
   authenticates_with_sorcery!
+  has_paper_trail
 end
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150406181652) do
+ActiveRecord::Schema.define(version: 20150406182054) do
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                                       null: false
@@ -42,5 +42,17 @@ ActiveRecord::Schema.define(version: 20150406181652) do
   add_index "users", ["remember_me_token"], name: "index_users_on_remember_me_token"
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token"
   add_index "users", ["unlock_token"], name: "index_users_on_unlock_token"
+
+  create_table "versions", force: :cascade do |t|
+    t.string   "item_type",      null: false
+    t.integer  "item_id",        null: false
+    t.string   "event",          null: false
+    t.string   "whodunnit"
+    t.text     "object"
+    t.datetime "created_at"
+    t.text     "object_changes"
+  end
+
+  add_index "versions", ["item_type", "item_id"], name: "index_versions_on_item_type_and_item_id"
 
 end
